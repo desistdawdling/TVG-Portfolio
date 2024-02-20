d667c80dccd2b52dcebbadfd3f6c09928da469db
Tina Velez-Grey <tvelezgrey@gmail.com>
2024-02-19 22:29:06 -0500

    sit changes

--- docs/portfolio_handlebars.js
+++
@@ -1,9 +0,0 @@
-function () {
-  var data = {
-    footer: "<div>Coded by Tina Velez-Grey | @DesistDawdling</div>",
-  }
-  var template = Handlebars.compule(document.querySelector("#template").innerHTML)
-  var complete = template(data, {
-    noEscape: true,
-  })
-}
