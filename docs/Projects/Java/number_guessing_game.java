b5afd14902684a1086760312784a5e1cdc06414c
desistdawdling <76758709+desistdawdling@users.noreply.github.com>
2024-02-20 16:23:45 -0500

    Update number_guessing_game.java
    created and tested in https://www.online-java.com/

--- docs/Projects/Java/number_guessing_game.java
+++ docs/Projects/Java/number_guessing_game.java
@@ -1,80 +1,71 @@
+
+/*
+Online Java - IDE, Code Editor, Compiler
+
+Online Java is a quick and easy tool that helps you to build, compile, test your programs online.
+*/
 import java.lang.Math;
 import java.util.Scanner;
 
-/*    If the guessed number is bigger than the actual number, the program will respond with the message that the guessed number is higher than the actual number.
-    If the guessed number is smaller than the actual number, the program will respond with the message that the guessed number is lower than the actual number.
-    If the guessed number is equal to the actual number or if the K trials are exhausted, the program will end with a suitable message.
+public class Main{
 
-Approach: Below are the steps:  
-
-    The approach is to generate a random number using Math.random() method in Java.
-    Now using a loop, take K input from the user and for each input print whether the number is smaller or larger than the actual number.
-    If within K trials the user guessed the number correctly, print that the user won.
-    Else print that he was not able to guess and then print the actual number.
-    */
-
-public static void main (String[] args){
- 
-    // Function that implements the
-    // number guessing game
-
-    public static void numberGuessingGame(){
+    public static void main (String [] numberGuessingGame){
         Scanner sc = new Scanner(System.in);
- 
-        // Generate the numbers
-        int number = 1 + (int)(100 * Math.random());
-        int K = 5;
-        int i, guess;
-        int newScore += (i * 10);
- 
-        System.out.println("A number is chosen between 1 to 100. Guess the number within 5 trials.");
- 
-        // Loop over number of trials (int K)
-        for (i = 0; i < K; i++) {
- 
-            System.out.println("Guess the number:");
- 
-            // Take input for guessing
-            guess = sc.nextInt();
- 
-            // If the number is guessed
-            if (number == guess) {
-                System.out.println(
-                    "Congratulations! You guessed the number.");
-                break;
-            }
-            // If number too high
-            else if (number > guess && (i != (K - 1))) {
-                System.out.println(
-                    "The number is greater than " + guess);
-            }
-            // If number too low
-            else if (number < guess && (i != (K - 1))) {
-                System.out.println(
-                    "The number is less than " + guess);
+        int newScore = 0;
+        int playAgain = 0;
+        boolean willPlayAgain = true;
+        if (playAgain != 0){
+            willPlayAgain = false;
+        }
+        else
+            while (willPlayAgain) {
+                newScore+=newScore;
+                // Generate the numbers
+                int number = 1 + (int)(100 * Math.random());
+                // Number of guesses allowed 
+                int K = 5;
+                int i = 0;
+                int guess;
+                // Score tracking
+     
+                System.out.println("A number is chosen between 1 and 100. Guess the number within 5 trials.");
+     
+                // Loop over number of trials (int K)
+                for (i = 0; i < K; i++) {
+     
+                    System.out.println("Guess the number:");
+     
+                    // Take input for guessing
+                    guess = sc.nextInt();
+     
+                    // If the number is guessed
+                    if (number == guess) {
+                        System.out.println("Congratulations! You guessed the number.");
+                        break;
+                    }
+                    // If number too high
+                    else if (number > guess && (i != (K - 1))) {
+                        System.out.println("The number is greater than " + guess);
+                    }
+                    // If number too low
+                    else if (number < guess && (i != (K - 1))) {
+                        System.out.println("The number is less than " + guess);
+                    }
+                }
+                newScore += (i * 10);
+                System.out.println("Your Score is: " + newScore);
+         
+                if (i == K) {
+                    System.out.println(
+                        "You have no more tries left.");
+         
+                    System.out.println("The number was " + number);
+                        
+                System.out.println("Do you want to play again? 0 for Yes or 1 for No: ");
+                playAgain = sc.nextInt();
+                    continue;
+                }
+                else break;
             }
         }
-        newScore += (i * 10);
-        System.out.println("Your Score is: " + newScore)
- 
-        if (i == K) {
-            System.out.println(
-                "You have no more tries left.");
- 
-            System.out.println(
-                "The number was " + number);
-        }
     }
- 
-    // Driver Code
-    public static void
-    main(String arg[])
-    {
- 
-        // Function Call
-        guessingNumberGame();
-    }
-}
-    
-
-
